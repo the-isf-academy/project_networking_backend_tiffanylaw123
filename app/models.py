d7fd04b164a97205e59ad07ef493e604cc9be319
tiffany <0014065@student.isf.edu.hk>
2024-10-14 11:56:01 +0800

    #today i finished working on models and i started with creating the mvp of my endpoints. #next i will work on the rest of the endpoints specifically one_recipe and change_recipe

--- app/models.py
+++ app/models.py
@@ -1,3 +1,43 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+class Recipe(Model):
+    cuisines = StringField()
+    name = StringField()
+    ingredients = StringField()
+    instructions = StringField()
+    likes = IntegerField()
+    views = IntegerField()
+    popularity = FloatField()
+
+    def json_response(self):
+        
+        return {
+            'id': self.id,
+            'cuisines': self.cuisines,
+            'name': self.name,
+            'ingredients': self.ingredients,
+            'instructions': self.instructions,
+            'likes': self.likes,
+            'views': self.views,
+            'popularity': self.popularity
+        }
+
+    def increase_likes(self):
+        self.likes += 1
+        self.save()
+    
+    def increase_views(self):
+        self.views += 1
+        self.save()
+
+    def change_recipe(self, ingredients, instructions):
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.likes = 0
+        self.save()
+
+    def calculate_popularity(self):
+        self.popularity_percentage = (self.likes/self.views) * 100
+        self.save()
--- app/views.py
+++ app/views.py
@@ -1,5 +1,30 @@
 # views.py
 
 from banjo.urls import route_get, route_post
+from .models import Recipe
 from settings import BASE_URL
 
+@route_get(BASE_URL + 'all')
+def all_recipes(args):
+    recipes_list = []
+
+    for recipe in Recipe.objects.all()
+        recipes_list.append(recipe.json_response())
+    
+    return {'recipe':recipes_list}
+
+@route_post(BASE_URL + 'new', args={'cuisines':str, 'name':str, 'ingredients':str, 'instructions':str})
+def new_recipe(args):
+    new_recipe = Recipe(
+        cuisines = args['cuisines'],
+        name = args['name'],
+        ingredients = args['ingredients'],
+        instructions = args['instructions'],
+        likes = 0,
+        views = 0,
+        popularity = 0
+    )
+
+    new_recipe.save()
+
+    return {'recipe': new_recipe.json_response()}
